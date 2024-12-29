61e479f921d8084a7e833d7a0cf96f31e7427145
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2024-12-29 15:15:17 +0700

    get status order

--- KoiGuardian.Models/Request/GHNRequest.cs
+++ KoiGuardian.Models/Request/GHNRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,13 +20,21 @@ namespace KoiGuardian.Models.Request
         public string to_address { get; set; }
         public string to_ward_code { get; set; }
         public int to_district_id { get; set; }
+        [Range(1, 50000, ErrorMessage = "Weight must be between 1 and 50000.")]//this mean maximum 50000gram
         public int weight { get; set; }
+        [Range(1, 200, ErrorMessage = "Weight must be between 1 and 200.")]//this mean maximum 200cm
         public int length { get; set; }
+        [Range(1, 200, ErrorMessage = "Weight must be between 1 and 200.")]//this mean maximum 200cm
         public int width { get; set; }
+        [Range(1, 200, ErrorMessage = "Weight must be between 1 and 200.")]//this mean maximum 200cm
         public int height { get; set; }
+        [AllowedValues(2, 5, ErrorMessage = "Service type ID must be either 2 or 5.")]//Default value: 2: E-commerce Delivery, 5: Traditional Delivery
         public int service_type_id { get; set; }
+        [AllowedValues(1, 2, ErrorMessage = "Payment type ID must be either 1 or 2.")]//Choose Who pay option 1: Shop/Seller. 2: Buyer/Consignee.
         public int payment_type_id { get; set; }
+        //only have 3 option : CHOTHUHANG, CHOXEMHANGKHONGTHU, KHONGCHOXEMHANG
         public string required_note { get; set; }
+        //this require when service_type_id you choose 5
         public List<Item> items { get; set; }
     }
 
---
+++ KoiGuardian.Models/ValidateAttributes/AllowedValuesAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiGuardian.Models.ValidateAttributes
+{
+    public class AllowedValuesAttribute : ValidationAttribute
+    {
+        private readonly int[] _allowedValues;
+
+        public AllowedValuesAttribute(params int[] allowedValues)
+        {
+            _allowedValues = allowedValues;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value is int intValue && !_allowedValues.Contains(intValue))
+            {
+                return new ValidationResult($"Value must be one of the following: {string.Join(", ", _allowedValues)}.");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+
+
+}

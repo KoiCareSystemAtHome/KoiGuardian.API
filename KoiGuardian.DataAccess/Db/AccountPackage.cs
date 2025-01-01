aa86206fd2492193f9558178d10571dc77ca1ae0
suongmaicute <maidtsse170085@fpt.edu.vn>
2025-01-01 23:17:26 +0700

    update

--- KoiGuardian.DataAccess/Db/AccountPackage.cs
+++ KoiGuardian.DataAccess/Db/AccountPackage.cs
@@ -9,10 +9,11 @@ namespace KoiGuardian.DataAccess.Db
     public class AccountPackage
     {
         public Guid AccountPackageid { get; set; }
-        public Guid AccountId { get; set; }
+        public string AccountId { get; set; }
         public Guid PackageId { get; set; }
         public DateTime PurchaseDate { get; set; }
 
         public virtual Package Package { get; set; }
+        public virtual User Member { get; set; }
     }
 }

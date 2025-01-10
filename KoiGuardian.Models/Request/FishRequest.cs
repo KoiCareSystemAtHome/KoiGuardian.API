0bbb4706d350eb70f61e45fd633c663ada65fa5a
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2025-01-11 00:46:12 +0700

    update fish and pond function update vs create

--- KoiGuardian.Models/Request/FishRequest.cs
+++ KoiGuardian.Models/Request/FishRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 
 namespace KoiGuardian.Models.Request
 {
@@ -7,7 +8,6 @@ namespace KoiGuardian.Models.Request
         public Guid? KoiID { get; set; }
         public Guid PondID { get; set; }
         public string Name { get; set; }
-        public string Image { get; set; }
         public string Physique { get; set; }
         public decimal Length { get; set; }  
         public string Sex { get; set; }
@@ -16,6 +16,7 @@ namespace KoiGuardian.Models.Request
         public decimal Weight { get; set; } 
         public string VarietyName { get; set; }
         public DateTime InPondSince { get; set; }
+        public IFormFile Image { get; set; }
         public decimal Price { get; set; }
         public required List<FishParam> RequirementFishParam { get; set; }
     }

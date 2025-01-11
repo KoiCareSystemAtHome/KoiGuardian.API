91e216125ee7d7ece6a0ed07351ebb4700c51cf1
suongmaicute <maidtsse170085@fpt.edu.vn>
2025-01-11 12:31:39 +0700

    allow cors

--- KoiGuardian.Api/Program.cs
+++ KoiGuardian.Api/Program.cs
@@ -79,6 +79,18 @@ builder.Services.AddSwaggerGen(option =>
             );
 
 builder.AppAuthentication();
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(p => p.AddPolicy(MyAllowSpecificOrigins, builder =>
+{
+    builder.WithOrigins(
+        "http://localhost:3000", "http://localhost:3001", "http://localhost:3002", "http://localhost:3003")
+           .AllowAnyMethod()
+           .AllowAnyHeader()
+           .AllowCredentials(); // Add this line to allow credentials
+
+    // Other configurations...
+}));
 builder.Services.AddAuthorization();
 
 var app = builder.Build();
@@ -95,8 +107,10 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 app.UseStaticFiles();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();

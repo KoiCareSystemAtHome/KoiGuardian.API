347357ce750d62cb4f5531477328bebe0cb50f12
suongmaicute <maidtsse170085@fpt.edu.vn>
2025-01-15 20:02:46 +0700

    update

--- KoiGuardian.DataAccess/Db/Mode.cs
+++ KoiGuardian.DataAccess/Db/Mode.cs
@@ -14,9 +14,11 @@ namespace KoiGuardian.DataAccess.Db
         public Guid ModeId { get; set; } 
         public Guid PondId { get; set; }
         public string ModeName { get; set; } = string.Empty;
-        public int Period { get; set; } // number of days in prediod 
+        public int Period { get; set; } // max day that the pond in period  
         public string WarningMessage { get; set; } = string.Empty;
         public float TemperatureUpper { get; set; } // temperature in standard unit : Cencius
         public float TemperatureLower { get; set; } // temperature in standard unit : Cencius
+        public int AgeFrom { get; set; } // từ số tháng tuổi 
+        public int AgeTo { get; set; } // tới số tháng tuồi
     }
 }

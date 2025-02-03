9d334418608f05b83eb4b1799069601b880f62e7
thanhphongne <thanhphongmainguyen@gmail.com>
2025-02-03 16:37:57 +0700

    fix salt

--- KoiGuardian.Api/Services/SaltCalculatorService.cs
+++ KoiGuardian.Api/Services/SaltCalculatorService.cs
@@ -1,29 +1,43 @@
 ﻿using KoiGuardian.Models.Request;
+using KoiGuardian.Models.Response;
+using System;
+using System.Threading.Tasks;
 
 namespace KoiGuardian.Api.Services
 {
     public interface ISaltCalculatorService
     {
-        double CalculateSalt(CalculateSaltRequest request);
+        Task<CalculateSaltResponse> CalculateSalt(CalculateSaltRequest request);
     }
 
     public class SaltCalculatorService : ISaltCalculatorService
     {
-        public double CalculateSalt(CalculateSaltRequest request)
-        {
-            
-            double requiredSaltPercent = request.StandardSaltPercent + request.SaltModifyPercent;
+        private readonly Dictionary<string, double> StandardSaltPercentDict = new Dictionary<string, double>
+{
+    { "Low", 0.3 },      
+    { "Medium", 0.5 },   
+    { "High", 0.7 }      
+};
 
-            
+        public async Task<CalculateSaltResponse> CalculateSalt(CalculateSaltRequest request)
+        {
+            // Lấy StandardSaltPercent từ dictionary, nếu không có thì mặc định là 0
+            StandardSaltPercentDict.TryGetValue(request.StandardSaltLevel, out double standardSalt);
+
+            double requiredSaltPercent = standardSalt + request.SaltModifyPercent;
             double totalSalt = request.WaterWeight * (requiredSaltPercent / 100);
 
-            
             if (totalSalt < request.LowerBound || totalSalt > request.UpperBound)
             {
                 throw new ArgumentOutOfRangeException("Lượng muối tính được nằm ngoài giới hạn cho phép.");
             }
 
-            return totalSalt;
+            return await Task.FromResult(new CalculateSaltResponse
+            {
+                PondId = request.PondId,
+                TotalSalt = totalSalt
+            });
         }
+
     }
 }
---
+++ KoiGuardian.Models/Response/CalculateSaltResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiGuardian.Models.Response
+{
+    public class CalculateSaltResponse
+    {
+      
+            public Guid PondId { get; set; }
+            public double TotalSalt { get; set; }
+       
+
+    }
+}

e0a69bf7407a60cd65c5248f5bec059c5b048acb
suongmaicute <maidtsse170085@fpt.edu.vn>
2025-02-11 21:34:42 +0700

    done

--- KoiGuardian.DataAccess/Db/RelSymptomDisease.cs
+++ KoiGuardian.DataAccess/Db/RelSymptomDisease.cs
@@ -5,12 +5,10 @@ public class RelSymptomDisease
     public Guid RelSymptomDiseaseId { get; set; }
     public Guid DiseaseId { get; set; }
     public Guid SymtompId { get; set; }
-    public Guid ParameterUnitId { get; set; }
     public float DiseaseUpper { get; set; }
     public float DiseaseLower { get; set; }
 
     public virtual Symptom? Symptom { get; set; }   
     public virtual Disease? Disease { get; set; }   
-    public virtual KoiStandardParam? Parameter { get; set; }
 }
 

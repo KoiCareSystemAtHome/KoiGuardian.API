852730e5eddaf6a561ceec71f46cff3f1829528e
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2025-02-17 14:30:05 +0700

    Create Order

--- KoiGuardian.Models/Request/OrderRequest.cs
+++ KoiGuardian.Models/Request/OrderRequest.cs
@@ -12,3 +12,24 @@ public class OrderFilterRequest
     public string RequestStatus { get; set; }
     public string SearchKey { get; set; }
 }
+
+public class CreateOrderRequest
+{
+    public Guid ShopId { get; set; }
+    public string AccountId { get; set; }
+    public string ShipType { get; set; }
+    public string Status { get; set; }
+    public decimal ShipFee { get; set; } // Lưu dạng số
+    public AddressDto Address { get; set; } // Đối tượng Address
+    public List<OrderDetailDto> OrderDetails { get; set; }
+}
+
+public class AddressDto
+{
+    public string ProvinceId { get; set; }
+    public string ProvinceName { get; set; }
+    public string DistrictId { get; set; }
+    public string DistrictName { get; set; }
+    public string WardId { get; set; }
+    public string WardName { get; set; }
+}

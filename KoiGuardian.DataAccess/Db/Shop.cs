a8ec93ddee39c3589fd317dcfa5880033ee5af7f
Nguyễn Minh Tâm <137066831+tententamy@users.noreply.github.com>
2025-02-24 13:22:58 +0700

    fix processing payment

--- KoiGuardian.DataAccess/Db/Shop.cs
+++ KoiGuardian.DataAccess/Db/Shop.cs
@@ -11,9 +11,10 @@ namespace KoiGuardian.DataAccess.Db
         public string ShopAddress { get; set; }
         public bool IsActivate { get; set; }
         public string BizLicences { get; set; }
+        public string UserId { get; set; }
 
 
-        
+
         public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
         public virtual ICollection<Blog> Blogs { get; set; } = new List<Blog>();
 
